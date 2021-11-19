7fe3c57e3c1a7d9b95b2ee7dc5783a0249c36014
giorgospapakon <papakon1999a@gmail.com>
2021-11-19 10:38:27 +0200

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ module.exports = {
         "bookmark-purple" : "#5267D7",
         "bookmark-red" : "#FA5959",
         "bookmark-blue" : "#242A45",
-        "bookmark-grey" : "9194A2",
-        "bookmark-white" : "f7f7f7",
+        "bookmark-grey" : "#9194A2",
+        "bookmark-white" : "#f7f7f7",
       },
     },
     fontFamily: {
